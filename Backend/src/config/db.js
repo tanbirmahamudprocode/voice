79492f0d3c4d30288251ef7e27a392445106f796
tanbirmahamudprocoder <tanbirmahamudprocoder@gmail.com>
2025-05-22 21:18:39 +0600

    first commit

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose')
 const connectdb = async ()=>{
   try {
     await mongoose.connect(process.env.MONGODB_URL)
+    console.log('Connected to MongoDB')
     
     
   } catch (error) {
+    console.log('MongoDB connection error',error)
     
   }
 }
